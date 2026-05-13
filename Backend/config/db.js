8bc0b497d2b46b178a0345174decba35c8070b7b
Srinath Palanisamy <srinathpalanisamy22@gmail.com>
2026-05-13 12:01:39 +0530

    fix

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,30 @@
+// import mysql from "mysql2";
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "K@vipriy@246810", // change this
+//   database: "ecommerce",
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("DB Error:", err);
+//   } else {
+//     console.log("MySQL Connected ✅");
+//   }
+// });
+
+// export default db;
+
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "K@vipriy@246810", // change this
-  database: "ecommerce",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "your_local_password",
+  database: process.env.DB_NAME || "ecommerce",
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
